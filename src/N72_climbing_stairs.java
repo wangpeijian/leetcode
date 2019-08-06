c7ee0656b940889bae3617a804fb1040c674b214
wangpeijian <wangpj@citic.com>
2019-08-06 13:33:35 +0800

    爬楼梯

--- src/N72_climbing_stairs.java
+++ src/N72_climbing_stairs.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-
 /**
  * @author wangpejian
  * @date 19-8-2 上午10:32
@@ -7,39 +5,38 @@ import java.util.HashMap;
 public class N72_climbing_stairs {
 
     public static void main(String[] args) {
-        System.out.println(new Solution72().climbStairs(3));
+        Solution72 s = new Solution72();
+        System.out.println(s.climbStairs(10));
+        System.out.println(s.memo);
     }
 
 }
 
 class Solution72 {
-
-    private int okStep = 0;
-    private HashMap<Integer, Integer> cache = new HashMap<>();
+    int memo[];
 
     public int climbStairs(int n) {
-
-        int s = foo(n);
-        return s;
-
+        this.memo = new int[n + 1];
+        return climb(n);
     }
 
-    int foo(int n) {
+    int climb(int i) {
 
-        if (n == 0) {
-            this.okStep++;
+        if (i < 0) {
+            return 0;
         }
 
-        if (n < 0) {
-            return -1;
+        if (i == 0) {
+            return 1;
         }
 
-        int s1 = foo(n - 1);
-        cache.put(n - 1, s1);
+        if (memo[i] > 0) {
+            return memo[i];
+        }
 
-        int s2 = foo(n - 2);
-        cache.put(n - 2, s2);
+        memo[i] = climb(i - 1) + climb(i - 2);
 
-        return this.okStep;
+        return memo[i];
     }
+
 }
