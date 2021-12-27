1498c17611e4481268da5f63b38f02f19e935581
王佩剑 <519955464@qq.com>
2021-12-27 11:03:07 +0800

    练习快排

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -3,7 +3,7 @@ package sort;
 public class QuickSort {
 
     public static void main(String[] args) {
-        int[] a = {6, 1, 2, 7, 9, 11, 4, 5, 10, 8};
+        int[] a = {6, 1, 2, 6, 7, 9, 11, 4, 5, 10, 8};
         new QuickSort.Solution().sortMe(a);
 
         for (int i : a) {
@@ -16,7 +16,7 @@ public class QuickSort {
     static class Solution {
 
         public void sortMe(int[] nums) {
-            sortMe(nums, 0, nums.length - 1);
+            sortTest(nums, 0, nums.length - 1);
         }
 
         void sort(int[] nums, int start, int end) {
@@ -89,6 +89,39 @@ public class QuickSort {
 
             sortMe(nums, start, l - 1);
             sortMe(nums, l + 1, end);
+        }
+
+        void sortTest(int[] nums, int start, int end) {
+
+            if (start >= end) {
+                return;
+            }
+
+            int k = nums[start];
+            int l = start;
+            int r = end;
+
+            while (l < r) {
+
+                while (nums[r] > k && l < r) {
+                    r--;
+                }
+
+                while (nums[l] <= k && l < r) {
+                    l++;
+                }
+
+                int tmp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = tmp;
+
+            }
+
+            nums[start] = nums[l];
+            nums[l] = k;
+
+            sortTest(nums, start, l - 1);
+            sortTest(nums, l + 1, end);
         }
     }
 
