34d7504b0c537380d2bbbf71c790db9d3ec24f88
王佩剑 <519955464@qq.com>
2021-12-28 17:33:48 +0800

    完成堆排序

--- src/sort/HeapSort.java
+++ src/sort/HeapSort.java
@@ -3,7 +3,7 @@ package sort;
 public class HeapSort {
 
     public static void main(String[] args) {
-        int[] a = {6, 1, 2, 6, 7, 9, 11, 4, 5, 10, 8};
+        int[] a = {20, 50, 20, 40, 70, 10, 80, 30, 60};
         new HeapSort.Solution().sortMe(a);
 
         for (int i : a) {
@@ -21,25 +21,36 @@ public class HeapSort {
         void sortTest(int[] nums, int start, int end) {
 
             // 1.先将堆调整为大顶堆
-            for (int i = 0; i < nums.length / 2; i++) {
-                adjust(nums, 0, nums.length - 1);
+            for (int i = nums.length / 2 - 1; i >= 0; i--) {
+                adjust(nums, i, nums.length);
             }
 
             // 2.每次循环将堆顶元素与尾节点交换,交换后对堆顶到尾结点-1的数据重新调整为大顶堆
             for (int i = nums.length - 1; i > 0; i--) {
                 swap(nums, 0, i);
-                adjust(nums, 0, i - 1);
+                adjust(nums, 0, i);
             }
         }
 
         void adjust(int[] nums, int start, int end) {
-            int l = start * 2 + 1;
-            int r = (start + 1) * 2;
+            for (int i = start; (i >= 0) && ((i * 2 + 1) <= end); ) {
+                int l = i * 2 + 1;
+                int r = (i + 1) * 2;
 
-            if(l < nums.length){
+                int p = l;
+                if (r <= end && nums[r] > nums[l]) {
+                    p = r;
+                }
 
+                if (p < end && nums[i] < nums[p]) {
+                    swap(nums, i, p);
+                } else {
+                    // 没发生交换,说明子节点都比当前节点小
+                    break;
+                }
+
+                i = p;
             }
-
         }
 
         void swap(int[] nums, int i, int j) {
