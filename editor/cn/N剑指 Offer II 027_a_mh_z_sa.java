a5025beb3b55bc84b0516ab23398a717d7ffe416
王佩剑 <519955464@qq.com>
2022-02-19 16:49:23 +0800

    练习

---
+++ editor/cn/N剑指 Offer II 027_a_mh_z_sa.java
@@ -0,0 +1,82 @@
+// 回文链表 2022-02-19 16:21:51
+
+//给定一个链表的 头节点 head ，请判断其是否为回文链表。 
+//
+// 如果一个链表是回文，那么链表节点序列从前往后看和从后往前看是相同的。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入: head = [1,2,3,3,2,1]
+//输出: true 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入: head = [1,2]
+//输出: false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表 L 的长度范围为 [1, 105] 
+// 0 <= node.val <= 9 
+// 
+//
+// 
+//
+// 进阶：能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？ 
+//
+// 
+//
+// 注意：本题与主站 234 题相同：https://leetcode-cn.com/problems/palindrome-linked-list/ 
+// Related Topics 栈 递归 链表 双指针 
+// 👍 27 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    ListNode p = null;
+    boolean r = true;
+
+    public boolean isPalindrome(ListNode head) {
+        if(head == null){
+            return r;
+        }
+
+        if(p == null){
+            p = head;
+        }
+
+        isPalindrome(head.next);
+
+        if(head.val != p.val){
+            r = false;
+            return r;
+        }
+
+        p = p.next;
+
+        return r;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
